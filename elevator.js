4fdf9b567463678e7505253517ab7aaf53344b88
Hinnerk <elhinni@gmail.com>
2015-03-24 19:45:02 +0100

    complete rewriteÄ

--- elevator.js
+++ elevator.js
@@ -1,24 +1,27 @@
 {          
     init: function(elevators, floors) {
         
-        function Elevator(reference, numberFloors) {
+        function Elevator(reference, numberFloors, id) {
             this.floorWasPressed = new Array(numberFloors);
             this.idle = true;   
             this.goingUp = true;
             this.ele = reference;
+            this.id = id;            
             for (var i = 0; i < numberFloors; i++)
                 this.floorWasPressed[i] = false;
-        }
+        }        
         
+        Elevator.prototype.describe = function() {
+            return "ID = " + this.id + ", idle = " + this.idle + ", goingUp = " + this.goingUp + ", currentFloor = " + this.currentFloor();
+        }    
+         
         Elevator.prototype.getObject = function() {
             return this.ele;
         };
         
-        Elevator.prototype.goToFloor = function(florNum) {            
-            this.idle = false;
-            var that = this;           
-            that.ele.goToFloor(florNum);                        
-            
+        Elevator.prototype.goToFloor = function(floorNum) {            
+            this.idle = false;            
+            this.ele.goToFloor(floorNum);            
         }
         
         Elevator.prototype.moveUp = function() {              
@@ -103,53 +106,89 @@
              this.goToNextFloor();       
         }
         
-        Elevator.prototype.isBelow = function(floorNumber) {        
+        Elevator.prototype.isBelow = function(floorNumber) {            
             return this.currentFloor() < floorNumber;
         }
         
-        Elevator.prototype.isOver = function(floorNumber) {        
+        Elevator.prototype.isOver = function(floorNumber) {             
             return this.currentFloor() > floorNumber;
         }
         
-        Elevator.prototype.distanceToFloor = function(floorNumber) {        
-            if (this.idle) {
+        Elevator.prototype.distanceToFloorMovingUpAfter = function(floorNumber) {        
+            if (this.idle) {                
                 return Math.abs(floorNumber - this.currentFloor());
             } else {
                 if (this.goingUp) {
-                    if (this.isBelow(floorNumber)) {
+                   if (this.isBelow(floorNumber)) {
                         return floorNumber -  this.currentFloor();
                     } else {
-                        return (floors.length - 1 -  this.currentFloor()) + (floors.length - 1 - floorNumber);
+                        return (floors.length - 1 -  this.currentFloor()) + floors.length - 1 + floorNumber;
                     }
-                } else {
+                } else {                    
                     if (this.isOver(floorNumber)) {
-                        return - floorNumber +  this.currentFloor();
+                        return - floorNumber +  this.currentFloor() + floors.length;
                     } else {
-                        return (this.currentFloor()) + (floorNumber);
+                        return this.currentFloor() +  floorNumber; //ok
                     }
                 }
             }
         }
         
+         Elevator.prototype.distanceToTop = function() {    
+             return floors.length - 1 -  this.currentFloor();
+         }
+        
+        Elevator.prototype.distanceToFloorMovingDownAfter = function(floorNumber) {        
+            if (this.idle) {                
+                return Math.abs(floorNumber - this.currentFloor());
+            } else {
+                if (this.goingUp) {
+                    if (this.isBelow(floorNumber)) {                    
+                        return this.distanceToTop() + (floors.length - 1 - floorNumber);
+                    } else {
+                        return this.distanceToTop() + (floors.length - 1 - floorNumber); //ok
+                    }
+                } else {
+                    if (this.isOver(floorNumber)) {
+                        return - floorNumber +  this.currentFloor();
+                    } else {
+                        return this.currentFloor() + (floors.length - 1) + ((floors.length - 1) - floorNumber);
+                    }
+                }
+            }
+        }
+        
+        Elevator.prototype.stop = function() {
+            console.log("stopping elevator " + this.id);
+            this.ele.stop();
+        }
+        
+          Elevator.prototype.hasSpace = function() { 
+            return this.ele.loadFactor() <= 0.5;
+        }
+        
         var myElevators = new Array(elevators.length);
         for (var i = 0; i < elevators.length; i++) {
             var elevator = elevators[i];
-            myElevators[i] = new Elevator(elevator, floors.length);
+            myElevators[i] = new Elevator(elevator, floors.length, i);
         }
-       
-        
+      
         function floorDownPressed(floor, floorNumber) {
             return function() {
                 var minDist = 999999;
                 var targetElevator;
-                for (var i = 0; i < myElevators.length; i++) {
+                for (var i = myElevators.length - 1; i >= 0; i--) {
                     var elevator = myElevators[i];
-                    if (elevator.distanceToFloor(floorNumber) < minDist) {
-                        minDist = elevator.distanceToFloor(floorNumber);
+                    if (elevator.hasSpace() && (elevator.distanceToFloorMovingDownAfter(floorNumber) < minDist)) {
+                        minDist = elevator.distanceToFloorMovingDownAfter(floorNumber);
                         targetElevator = elevator;
                     }
                 }
-                targetElevator.personPressedFloor(floorNumber);
+                if (typeof targetElevator !== 'undefined') {                   
+                    if (minDist == 0)
+                        targetElevator.stop();
+                    targetElevator.personPressedFloor(floorNumber);               
+                }
             };
         }
         
@@ -159,47 +198,64 @@
                 var targetElevator;
                 for (var i = 0; i < myElevators.length; i++) {
                     var elevator = myElevators[i];
-                    if (elevator.distanceToFloor(floorNumber) < minDist) {
-                        minDist = elevator.distanceToFloor(floorNumber);
+                    if (elevator.hasSpace() && (elevator.distanceToFloorMovingUpAfter(floorNumber) < minDist)) {
+                        minDist = elevator.distanceToFloorMovingUpAfter(floorNumber);
                         targetElevator = elevator;
                     }
                 }
-                targetElevator.personPressedFloor(floorNumber);
+                if (typeof targetElevator !== 'undefined') {                        
+                    if (minDist == 0)
+                        targetElevator.stop();
+                    targetElevator.personPressedFloor(floorNumber);
+                }
             };
         }
         
         function onIdleElevator(elevator) {            
             return function() {                     
-                 elevator.idle = true;
-                 elevator.goToNextFloor();         
+                 elevator.idle = true;                
+                 elevator.goToNextFloor();   
+                 if (elevator.idle) {
+                     if (elevator.currentFloor() < 1) {
+                         if (floors.length > 4) {
+                             console.log("elevator idle at bottom. moving to floor 1");
+                             elevator.personPressedFloor(1);
+                         }
+                     }
+                     else  if (elevator.currentFloor() >= floors.length - 2) {
+                         if (floors.length > 4) {
+                             elevator.personPressedFloor(floors.length - 3);
+                             console.log("elevator idle at top. moving to floor " + floors.length - 3);
+                         }
+                     }
+                 }                 
             };        
         }
         
         function onStoppedElevator(elevator) {
             return function(floorNum) {
-               elevator.floorWasPressed[floorNum] = false;
+                elevator.floorWasPressed[floorNum] = false;                
             };
         }                              
         
         function onFloorButtonPressed(elevator) {
             return function(floorNum) {                       
-                elevator.personPressedFloor(floorNum);   
-                elevator.goToNextFloor();       
+                elevator.personPressedFloor(floorNum);                   
             };
         }
         
          function onFloorPassing(elevator) {
              return function(floorNum, direction) {                 
-                 if (elevator.floorWasPressed[floorNum]) 
-                     elevator.goToFloor(floorNum);
+                 if (elevator.floorWasPressed[floorNum] && elevator.hasSpace()) 
+                     elevator.stop();
             };
             
          }
         
         function distributeElevators(elevators, floors) {
-            for (var i = 1; i < elevators.length; i++) {
+            for (var i = 0; i < elevators.length; i++) {
                 var elevator = elevators[i];
-                var targetFloor = Math.floor(i / elevators.length * floors.length);
+                var targetFloor = Math.floor(i  / (elevators.length) * floors.length);
                 elevator.goToFloor(targetFloor);
             }
         }
